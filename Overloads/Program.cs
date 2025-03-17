b30f4d6b12eea140527212ff7af6df1ccb962eb7
GeorgeSmileyface <104126452+GeorgeSmileyface@users.noreply.github.com>
2025-03-17 08:44:57 +0100

    Add project files.

---
+++ Overloads/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Overloads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StylishPrint("Hello, World!");
+            StylishPrint("Hello, World!", 200);
+        }
+
+        static void StylishPrint(string toPrint, int timeBetweenLetters)
+        {
+            foreach (char letter in toPrint)
+            {
+                Console.Write(letter);
+                Thread.Sleep(timeBetweenLetters);
+            }
+            Console.WriteLine();
+        }
+        static void StylishPrint(string toPrint)
+        {
+            foreach (char letter in toPrint)
+            {
+                Console.Write(letter);
+                Thread.Sleep(60);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
